37fc4cc4be3be81ec19a0c4a01e90c1bc7b1ac59
MrsKing2023 <smarshall84@yahoo.com>
2023-09-12 16:34:22 -0400

    stopped at PositionType

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -18,7 +20,25 @@ public class PositionType {
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
-    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
+    @Override
+    public String toString() {
+        return "PositionType{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PositionType that)) return false;
+        return getId() == that.getId() && Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getValue());
+    }
+// TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
 
     // Getters and Setters:
